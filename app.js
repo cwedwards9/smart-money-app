951bd1432468a33effc19d5a125ad73f9059e6fe
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-21 13:57:03 -0500

    Set up express handlebars, import user routes file.

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
+// Use public directory
 app.use(express.static("public"));
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import Routes
+require("./routes/user-routes")(app);
 
 
 app.listen(PORT, () => {
