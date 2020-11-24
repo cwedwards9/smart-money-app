63cc193e2d7ecf89f1f834c4c419b7a6352e0ff3
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-23 21:34:21 -0500

    Add GET routes for login and register page. Set up POST route for creating new user.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,14 +1,29 @@
-const db = require("../models");
+let db = require("../models");
 
 module.exports = function(app) {
+    // Home page
     app.get("/", (req, res) => {
         res.render("home");
     });
 
-    app.get("/users", (req, res) => {
-        db.User.findAll({}).then(function(data) {
+    // Login page
+    app.get("/login", (req, res) => {
+        db.User.findAll({}).then(data =>{
             console.log(data);
-            // res.render("users", {users: data});
+            res.render("login", {users: data});
           });
     });
+
+    // Register page
+    app.get("/register", (req, res) => {
+        res.render("register");
+    });
+
+    // POST route for registering new user
+    app.post("/register", (req, res) => {
+        db.User.create(req.body).then(() => {
+            res.redirect("/login");
+        });
+    });
+
 }
