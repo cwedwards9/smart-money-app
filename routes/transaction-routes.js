c5dbe8ed3b1611cdfdd816b10df88d4ffb33f6f6
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-23 21:33:09 -0500

    Initial commit

---
+++ routes/transaction-routes.js
@@ -0,0 +1,14 @@
+let db = require("../models");
+
+module.exports = function(app){
+    // Get transactions for specific user, 
+    app.get("/user/transactions/:id", (req, res) => {
+        
+    });
+
+    // POST new transaction
+    app.post("/transaction", (req, res) => {
+
+    });
+
+}
