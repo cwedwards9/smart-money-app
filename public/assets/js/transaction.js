cda3cdac75ea1584a619e1937fd773b77dd9927d
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-28 13:48:38 -0500

    Add function calculateTotalAmount to add the amount of each transaction together to get a total amount spent to display on the page.

--- public/assets/js/transaction.js
+++ public/assets/js/transaction.js
@@ -18,3 +18,25 @@ $("#createTransaction").on("submit", e => {
         location.reload();
     });
 });
+
+calculateTotalAmount();
+
+function calculateTotalAmount() {
+    // Get all elements with amountDisplay class
+    let amount = $(".amountDisplay");
+    // Create a new array from array like object
+    let amountArr = Array.from(amount);
+
+    // Map through array and make a new array with all HTML within each element
+    let newArray = amountArr.map(val => {
+        return Number(val.innerText);
+    });
+
+    // Add up each value to get a total number for the total amount spent
+    let totalAmount = newArray.reduce((acc, curVal) => {
+        return acc + curVal;
+    });
+
+    // Display the total amount spent in the element
+    $("#amountSpent").html(totalAmount);
+}
