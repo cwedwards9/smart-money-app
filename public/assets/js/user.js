976a203e53917ac5b96030ae36babeb1e684fc12
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-29 17:58:57 -0500

    Add event listener to the login button on the home page to get all users and append them to the list in the login modal/pop up.

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -19,7 +19,22 @@ $("#createUser").on("submit", (e) => {
         data: newUser
     }).then(() => {
         // Once the user is registered, they will be redirected to the 'login' page
-        window.location.replace("/login");
+        window.location.reload();
     });
 
 });
+
+
+// GET request to get all users from the database
+$("#loginBtn").on("click", () => {
+    $.get("/login")
+    .done(data => {
+        console.log(data);
+        for(let i = 0; i < data.length; i++){
+            let id = data[i].id;
+            let name = data[i].f_name + " " + data[i].l_name;
+            let user = $("<li><a href='/user/transactions/"+ id + "'>" + name + "</a></li>");
+            $("#user-login").append(user);
+        }
+    });
+})
