cd438bca6e25a9546dc9c2ccef4cb27641712dce
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-01 14:17:41 -0500

    Move scroll javascript code from the now deleted main.js file to home.js file.

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,4 +1,44 @@
+// Scroll effect
+(function ($) {
+    "use strict";
+    /*----------  Scroll to top  ----------*/
+    function scrollToTop() {
+      var $scrollUp = $("#scroll-top"),
+        $lastScrollTop = 0,
+        $window = $(window);
+      $window.on("scroll", function () {
+        var st = $(this).scrollTop();
+        if (st > $lastScrollTop) {
+          $scrollUp.removeClass("show");
+        } else {
+          if ($window.scrollTop() > 200) {
+            $scrollUp.addClass("show");
+          } else {
+            $scrollUp.removeClass("show");
+          }
+        }
+        $lastScrollTop = st;
+      });
+      $scrollUp.on("click", function (evt) {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        evt.preventDefault();
+      });
+    }
+    scrollToTop();
+    /*=====  End of menu sticky and scroll to top  ======*/
+  /*----------  background image  ----------*/
+    var bgSelector = $(".bg-img");
+    bgSelector.each(function (index, elem) {
+      var element = $(elem),
+        bgSource = element.data("bg");
+      element.css("background-image", "url(" + bgSource + ")");
+    });
+    /*=====  End of background image  ======*/
+  })(jQuery);
 
+  
+
+// Modal functionality for registering and logging in
 let modalBtns = [...document.querySelectorAll(".button")];
 modalBtns.forEach(function (btn) {
     btn.onclick = function () {
@@ -7,6 +47,7 @@ modalBtns.forEach(function (btn) {
             .style.display = "block";
     }
 });
+
 let closeBtns = [...document.querySelectorAll(".close")];
 closeBtns.forEach(function (btn) {
     btn.onclick = function () {
@@ -14,6 +55,7 @@ closeBtns.forEach(function (btn) {
         modal.style.display = "none";
     }
 });
+
 window.onclick = function (event) {
     if (event.target.className === "modal") {
         event.target.style.display = "none";
