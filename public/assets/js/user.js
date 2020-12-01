a01741f0ad71aa6e37eecd22a28570a3c5423a29
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-01 12:52:30 -0500

    Add comments.

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,6 +1,7 @@
 $("#createUser").on("submit", (e) => {
     e.preventDefault();
 
+    // Select values from the form
     let firstName = $("#firstNameInput").val().trim();
     let lastName = $("#lastNameInput").val().trim();
     let monthlyBudget = $("#monthlyBudget").val().trim();
@@ -16,11 +17,12 @@ $("#createUser").on("submit", (e) => {
         budget: monthlyBudget
     }
 
+    // Post user data to the /register route
     $.ajax("/register", {
         type: "POST",
         data: newUser
     }).then(() => {
-        // Once the user is registered, they will be redirected to the 'login' page
+        // Once the user is registered, the page will reload
         window.location.reload();
     });
 
@@ -31,10 +33,14 @@ $("#createUser").on("submit", (e) => {
 $("#loginBtn").on("click", () => {
     $.get("/login")
     .done(data => {
+        // Remove list of users before repopulating them in the list
         $("#user-login").empty();
+        // Loop through each user in the database and get the id, first and last name
         for(let i = 0; i < data.length; i++){
             let id = data[i].id;
             let name = data[i].f_name + " " + data[i].l_name;
+            
+            // Create a list item with the user's name and a link to their transactions page
             let user = $("<li><a href='/user/transactions/"+ id + "'>" + name + "</a></li>");
             $("#user-login").append(user);
         }
