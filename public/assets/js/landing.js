e32e6093a0c32971e31dab613404c00cf8f6c4df
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-05 13:37:45 -0500

    Add event listener to the update budget modal to send a PUT request to update the user's budget.

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,5 +1,18 @@
 // On submit of the edit button, send the updated budget to the '/user' PUT route
 $("#editBudget").on("submit", (e) => {
     e.preventDefault();
+
+    let updatedBudget = {
+        budget: $("#updatedBudgetInput").val().trim(),
+        id: $("#userid").html().trim()
+    }
+
+    // Send updated budget as PUT request
+    $.ajax("/budget", {
+        method: "PUT",
+        data: updatedBudget
+    }).then(() => {
+        location.reload();
+    });
     
 });
