b8f5baa980e9be09a6338f6379f04f650ac1ec1e
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-31 15:11:32 -0500

    Require bcrypt. On new user creation, hash the password automatically. Create a  method to validate password with hashed password in our database.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,20 @@
+const bcrypt = require("bcryptjs");
+
+
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         f_name: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -25,6 +40,17 @@ module.exports = function(sequelize, DataTypes) {
     },{
         freezeTableName: true
     });
+
+    // Custom method for validating the inputted password, comparing it to the hashed password in our database
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+      };
+      
+    // Before we create a User, we automatically hash their password
+    User.addHook("beforeCreate", function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(12), null);
+    });
+
 
     User.associate = function(models) {
         // Associating User with Transactions/Bills, if a user is deleted, the transactions/bills for that user are also deleted
