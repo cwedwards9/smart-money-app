caab672bb5ddb1974f6505168df53672de0c9bb1
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-31 17:15:28 -0500

    Import/require bcryptjs, express-session, passport, passport-local.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
+
+const session = require("express-session");
+const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 8080;
 
-let db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +15,14 @@ app.use(express.json());
 // Use public directory
 app.use(express.static("public"));
 
+// Use sessions in order to use persistent login sessions
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // Set Handlebars
-let exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
