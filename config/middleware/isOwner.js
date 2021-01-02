177286c42167caa3380033d9ecee363fa57d6877
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-02 12:51:17 -0500

    Middleware for restricting routes if a user is not the owner of the page.

---
+++ config/middleware/isOwner.js
@@ -0,0 +1,19 @@
+const db = require("../../models");
+
+// Middleware for restricting routes if a user is not the author/owner of the page
+module.exports = function(req, res, next) {
+    db.findOne({
+        where: {
+            id: req.body.id
+        }
+    }).then(user => {
+        // If the user is not the owner, redirect them
+        if(user.dataValues.id !== req.user.id) {
+            console.log("You do not have permission to do that!");
+            return res.redirect("/");
+        }
+
+        // If the user is the author/owner, continue with the request 
+        return next();
+    });
+}
